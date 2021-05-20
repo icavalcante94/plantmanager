910a1f0727a53971ca840437945afd67a1f98980
Iago Cavalcante <iago.cavalcante.94@gmail.com>
2021-05-19 22:06:00 -0400

    criado a segunda tela e botão padrão

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,38 +1,34 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps} from 'react-native';
+import { 
+    TouchableOpacity,
+    Text,
+    StyleSheet
+} from 'react-native';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-
-interface ButtonProps extends TouchableOpacityProps{
-    title: string;
-}
-
-export function Button({title, ...rest} : ButtonProps){
-    return(
-        <TouchableOpacity 
-            style={styles.button}
-            activeOpacity={0.7} //controla efeito do clique
-            {...rest}
-        >
-            <Text style={styles.buttonText}>
-                { title }
+export function Button(){
+    return (
+        <TouchableOpacity style={styles.container}>
+            <Text style={styles.text}>
+                Confirmar
             </Text>
         </TouchableOpacity>
     )
 }
 
-const styles = StyleSheet.create({
-    button: {
+const styles = StyleSheet.create ({
+    container: {
         backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
         height: 56,
-        width: 56
+        borderRadius: 16, 
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    buttonText: {
-        color: colors.white
+    text: {
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: fonts.heading
     }
-
-});
+})
