d01f9cfb71193ba18ba3f6587b9e7c1657d3f37f
Iago Cavalcante <iago.cavalcante.94@gmail.com>
2021-05-19 21:27:45 -0400

    finalização tela welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,42 +1,59 @@
-import React, {useState} from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, Platform, StatusBar } from 'react-native'
+import React from 'react';
+import { 
+    SafeAreaView,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions,
+    View
+} from 'react-native'
+import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts';
 
-//usando para que o texto não fique atrás do status bar
+// inicío da declaração exigida para configuração usada para que o texto não fique atrás do status bar
 import Constants from 'expo-constants'; 
 const statusBarHeight = Constants.statusBarHeight
-
+// fim da declaração para configuração usada para que o texto não fique atrás do status bar
 export function Welcome(){
-    const [visible, setVisible ] = useState(false);
-
-    function handleVisibility(){
-        setVisible(true)
-    }
-
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
+            <View style={styles.wrapper}> 
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas {'\n'}
+                    de forma fácil
+                </Text>
 
-            {
-                visible &&
-                <Image source={wateringImg} style={styles.image}/>
-            }
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image}
+                    resizeMode='contain'
+                />
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. 
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. 
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <Button title="Imagem" onPress={handleVisibility} />
+                <TouchableOpacity 
+                    style={styles.button}
+                    activeOpacity={0.7} //controla efeito do clique
+                >
 
+                    <Text>
+                        <Feather 
+                            name="chevron-right"
+                            style={styles.buttonIcon}
+                        />
+                    </Text>
+
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -45,10 +62,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between',
-        marginTop: statusBarHeight ,
-        marginBottom: 20,
-                
+        justifyContent: 'space-around',
+        marginTop: statusBarHeight , //configuração usada para que o texto não fique atrás do status bar
+        marginBottom: 20, //configuração usada para que o texto não fique atrás do status bar
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
         fontSize: 32,
@@ -56,17 +78,31 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 38
     },
     subtitle: {
         fontSize: 17,
         textAlign: 'center',
         color: colors.heading,
-        // borderWidth: 1,
-        paddingHorizontal: 20 // distancia da borda
+        paddingHorizontal: 20, // distancia da borda
+        fontFamily: fonts.text
     },
     image: {
-        height: 292,
-        width: 284
+        height: Dimensions.get('window').width * 0.7 // ajuste de imagem conforme o tamanho da tela, reponsivel
     },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonIcon: {
+        color: colors.white,
+        fontSize: 24
+    }
 
 });
